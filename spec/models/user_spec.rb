0248954c483474c3d5a03dc495bbac5ea9ba0dd4
mike8730 <razerrush5v@gmail.com>
2025-08-04 11:05:44 +0900

    テストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
-  
+
   describe 'ユーザー新規登録' do
     context '登録できるとき' do
       it '全ての項目が正しく入力されていれば登録できる' do
@@ -69,11 +69,23 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Last name can't be blank")
       end
+      
+      it 'last_nameが全角でないと登録できない' do
+        @user.last_name = 'Yamada'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name is invalid")
+      end
 
       it 'first_nameが空では登録できない' do
         @user.first_name = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it 'first_nameが全角でないと登録できない' do
+        @user.first_name = "Taro"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid")
       end
 
       it 'last_name_kanaが空では登録できない' do
