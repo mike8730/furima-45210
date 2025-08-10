a25459e72d2d241dfdc8caf0a037f6570740ff46
mike8730 <razerrush5v@gmail.com>
2025-08-10 15:33:52 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :name, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 300,
+                                    less_than_or_equal_to: 9_999_999 }
+  validates :description, presence: true
+  validates :user, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :image, presence: true
+
   belongs_to :user
   has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :category
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :condition
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :shipping_fee_burden
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :Prefecture
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
   belongs_to :shipping_day
 end
+
