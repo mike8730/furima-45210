ceb2c7a77c9a2115cf4882393a5ccbe259020086
mike8730 <razerrush5v@gmail.com>
2025-08-10 15:05:24 +0900

    アクティブハッシュの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_fee_burden
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_day
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,6 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
 
+  has_many :items
+
 end
