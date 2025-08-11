bd4969a685afe7fa5cb279923375ec7e1e3d8453
mike8730 <razerrush5v@gmail.com>
2025-08-11 10:23:03 +0900

    手数料・利益計算機能を実装

---
+++ app/javascript/profit_fee.js
@@ -0,0 +1,26 @@
+document.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById('item-price')
+  const addTaxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  if (!priceInput) return;
+
+  priceInput.addEventListener('input', () => {
+    const value = priceInput.value;
+    const price = parseInt(value, 10)
+  
+
+  if (isNaN(price) || price < 300 || price > 9999999) {
+      addTaxPrice.textContent = '';
+      profit.textContent = '';
+      return;
+    }
+  
+  const fee = Math.floor(price * 0.1); 
+  const gain = price - fee;
+
+  addTaxPrice.textContent = fee.toLocaleString();
+  profit.textContent = gain.toLocaleString();
+  })
+})
+
