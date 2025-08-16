09f74f4b84556a21db01dded354b1823f31e34fe
mike8730 <razerrush5v@gmail.com>
2025-08-16 19:34:52 +0900

    ルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ get "up" => "rails/health#show", as: :rails_health_check
 
 root to: "items#index"
 
-resources :items, only:[:index, :new, :create]
+resources :items, only:[:index, :new, :create, :show]
 
 end
