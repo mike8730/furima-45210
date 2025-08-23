6a0e1de19e4e32205ee487e068262ca7d83d3bed
mike8730 <razerrush5v@gmail.com>
2025-08-23 18:40:37 +0900

    ordersコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,39 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+
+  def index
+    @order_shipping = OrderShipping.new
+  end
+
+  def create
+    @order_shipping = OrderShipping.new(order_shipping_params)
+    if @order_shipping.valid?
+      pay_item  # 決済処理
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_shipping_params
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+          .merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_shipping_params[:token],
+      currency: 'jpy'
+    )
+  end
 end
