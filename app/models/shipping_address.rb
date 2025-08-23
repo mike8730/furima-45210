6ecf26ec75faf3909dcd13d09f96cf57837b2ca0
mike8730 <razerrush5v@gmail.com>
2025-08-23 20:41:31 +0900

    shipping_addressモデルの編集

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,12 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
-  belongs_to :prefecture
+  belongs_to_active_hash :prefecture
+
+  with_options presence: true do           
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
 end
