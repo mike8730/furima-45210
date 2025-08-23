bdba12227229a47ac27313cb53f6cc6086b9819f
mike8730 <razerrush5v@gmail.com>
2025-08-23 19:23:38 +0900

    ビューファイルの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_shipping = OrderShipping.new(order_shipping_params)
     if @order_shipping.valid?
-      pay_item  # 決済処理
+      pay_item  
       @order_shipping.save
       redirect_to root_path
     else
