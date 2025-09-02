d58fc4bd6cfa94857f63b8aa5f439091f3c052db
mike8730 <razerrush5v@gmail.com>
2025-09-02 23:54:11 +0900

    商品購入機能の再修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,14 +14,11 @@ const pay = () => {
   const form = document.getElementById('charge-form');
 
   form.addEventListener('submit', (e) => {
-    e.preventDefault(); // 通常送信を防ぐ
+    e.preventDefault(); 
 
     payjp.createToken(numberElement).then((response) => {
       if (response.error) {
-        // token は作らず、そのまま送信 → Rails 側でバリデーション
-        console.log('カード情報にエラーがあります:', response.error.message);
       } else {
-        // トークン生成成功時のみ hidden input を追加
         const token = response.id;
         form.insertAdjacentHTML(
           'beforeend',
@@ -29,12 +26,10 @@ const pay = () => {
         );
       }
 
-      // token があってもなくても送信 → Rails 側でエラー表示可能
       form.submit();
     });
   });
 };
 
-// Turbo 対応：ページ読み込み時および再描画時に呼び出す
 window.addEventListener('turbo:load', pay);
 window.addEventListener('turbo:render', pay);
